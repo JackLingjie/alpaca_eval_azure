1ddbb7cf7b949c6919acb836ed9af9a299650b70
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2024-08-13 07:01:03 -0700

    generate tulu answer and make judgement

--- bash_script.sh
+++ bash_script.sh
@@ -2,6 +2,27 @@
 alpaca_eval evaluate_from_model \
   --model_configs 'tulu-sft-Llama3_1-8B' \
   --annotators_config 'alpaca_eval_gpt4_turbo_fn'  
-
+# eval
 alpaca_eval --model_outputs './data/Meta-Llama-3.1-8B.json' \
 --annotators_config 'alpaca_eval_gpt4_turbo_fn'    
+
+# eval
+alpaca_eval --model_outputs './data/Meta-Llama-3.1-8B-Instruct.json' \
+--annotators_config 'alpaca_eval_gpt4_turbo_fn'    
+
+
+
+## generate output
+### llama3-instruct 
+python generate_response.py --model-name "Meta-Llama-3.1-8B-Instruct"
+
+python generate_response.py --model-path "/mnt/lingjiejiang/textual_aesthetics/model_checkpoint/sft_merge_checkpoints" --model-name "tulu-lora-sft-llama3-8b-base"
+
+alpaca_eval --model_outputs './data/tulu-lora-sft-llama3-8b-base.json' \
+--annotators_config 'alpaca_eval_gpt4_turbo_fn'
+
+
+CUDA_VISIBLE_DEVICES=1 python generate_response.py --model-path "/mnt/lingjiejiang/textual_aesthetics/model_checkpoint/sft_merge_checkpoints" --model-name "tulu-dpo-llama3-8b-base"
+
+alpaca_eval --model_outputs './data/tulu-dpo-llama3-8b-base.json' \
+--annotators_config 'alpaca_eval_gpt4_turbo_fn'   
---
+++ generate_response.py
@@ -0,0 +1,71 @@
+from vllm import LLM, SamplingParams
+from tqdm import tqdm
+import datasets
+import json
+import argparse 
+
+parser = argparse.ArgumentParser(description='Set model name.')  
+parser.add_argument('--model-name', type=str, required=True, help='Name of the model to use')  
+parser.add_argument('--model-path', type=str, default="/home/lidong1/jianglingjie/LLama-Factory/model_checkpoint/huggingface", help='Dir of the model to use') 
+args = parser.parse_args()  
+path_dir = args.model_path
+model_name = args.model_name 
+
+template = "{{ '<|begin_of_text|>' }}{% if messages[0]['role'] == 'system' %}{% set system_message = messages[0]['content'] %}{% endif %}{% if system_message is defined %}{{ '<|start_header_id|>system<|end_header_id|>\n\n' + system_message + '<|eot_id|>' }}{% endif %}{% for message in messages %}{% set content = message['content'] %}{% if message['role'] == 'user' %}{{ '<|start_header_id|>user<|end_header_id|>\n\n' + content + '<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n' }}{% elif message['role'] == 'assistant' %}{{ content + '<|eot_id|>' }}{% endif %}{% endfor %}"
+
+
+# model_name = "Meta-Llama-3.1-8B-Instruct"
+# model_name = "Meta-Llama-3.1-8B"
+# Create an LLM.
+llm = LLM(model=f"{path_dir}/{model_name}")
+
+print(f"model name: {model_name}")
+print(f"model_path: {path_dir}/{model_name}")
+
+tokenizer = llm.get_tokenizer()
+if tokenizer.chat_template is None:
+    tokenizer.chat_template = template
+    tokenizer.chat_template = tokenizer.chat_template.replace("<|eot_id|>", tokenizer.eos_token)
+    tokenizer.chat_template
+    print(f"tokenizer.chat_template: {tokenizer.chat_template}")
+    print("tokenizer is None, use setted template")
+else:
+    print("use original template")
+# messages = tokenizer.apply_chat_template(messages, tokenize=False)
+
+gen_kwargs_vllm = {
+    "max_tokens": 1024,
+    "top_p": 0.9,
+    "top_k": 50,
+    "temperature": 0.0,
+    "repetition_penalty": 1.0,
+}
+gen_kwargs_vllm['stop_token_ids'] = [tokenizer.eos_token_id, tokenizer.convert_tokens_to_ids("<|end_of_text|>")]
+sampling_params = SamplingParams(**gen_kwargs_vllm)
+
+eval_set = datasets.load_dataset("tatsu-lab/alpaca_eval", "alpaca_eval")["eval"]
+
+def convert_to_message(example):  
+    messages = [{"role": "user", "content": example["instruction"]}]  
+    example["messages"] = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)  
+    return example  
+eval_set = eval_set.map(convert_to_message)
+# eval_set['messages']
+outputs = llm.generate(eval_set['messages'], sampling_params)
+outputs_text = [x.outputs[0].text for x in outputs]
+eval_set = eval_set.remove_columns(["output"])  # Remove the existing 'output' column if it exists  
+eval_set = eval_set.remove_columns(["messages"])
+eval_set = eval_set.add_column("output", outputs_text)  
+def rename_generator(sample):
+    sample['generator'] = f"{model_name}"
+    return sample
+eval_set = eval_set.map(rename_generator)
+eval_set.to_json(f"{model_name}.jsonl", batch_size=128, num_proc=8)
+
+## save data
+
+export_dataset = eval_set
+export_data_list = [dict(row) for row in export_dataset]
+print(f"export data length {len(export_data_list)}")
+with open(f'./data/{model_name}.json', 'w', encoding='utf-8') as f:  
+    json.dump(export_data_list, f, ensure_ascii=False, indent=4)  
