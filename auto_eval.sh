ad7981be196c758f3235de2078a24cc713ed84a3
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2024-08-27 00:38:11 -0700

    eval wild chat

--- auto_eval.sh
+++ auto_eval.sh
@@ -2,23 +2,35 @@
 set -x
 
 export CUDA_VISIBLE_DEVICES=3
+
 # 参数列表  
 PARAMS=(  
+    "wildchat_v1_8b_2048_default_template"
     # "tulu_v2_8b_2048_default_template_sft" 
     # "tulu_v2_8b_2048_default_template_dpo" 
-  "tulu-2-7b"  
-  # "allenai/tulu-2-dpo-7b"
-  # "Meta-Llama-3.1-8B-Instruct"
+    # "tulu-2-7b"  
+    # "tulu_lora_sft_base_template_8b" 
+    # "tulu_v2_8b_base_template_dpo" 
+    # "tulu_v2_8b_bsz64_default_template_dpo" 
+    # "tulu_lora_sft_default_template_8b" 
+    # "Meta-Llama-3.1-8B-Instruct"
+    # "Meta-Llama-3.1-8B"
   # 添加更多参数  
 )  
 
 # 原始日志文件名  
 LOG_FILE="evaluation_log"  
-  
+counter=0
+
 # 遍历模型列表，生成新的文件名  
-for model in "${PARAMS[@]}"; do  
+for model in "${PARAMS[@]}"; do 
+    if [ $counter -ge 3 ]; then  
+        break  
+    fi
     # 拼接模型名到log_file后  
     LOG_FILE="${LOG_FILE}_${model}"  
+
+    counter=$((counter + 1)) 
 done  
   
 # 最终的文件名  
@@ -26,6 +38,8 @@ LOG_FILE="./eval_logs/${LOG_FILE}.txt"
 
 # 清空日志文件  
 > "$LOG_FILE" 
+
+echo $LOG_FILE
 
 # 遍历参数列表  
 for PARAM in "${PARAMS[@]}"; do  
