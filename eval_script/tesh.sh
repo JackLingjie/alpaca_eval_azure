2562879d5ba1ac53f78d99cbcfa75c6d368f913e
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-01-07 12:12:23 +0000

    eval new models

---
+++ eval_script/tesh.sh
@@ -0,0 +1,3 @@
+python generate_response_fullft.py \
+    --model-path "/mnt/lingjiejiang/textual_aesthetics/exp/saves/deepspeed_test/fullft_lr5e6_e3_fx/sft"\
+    --model-name "deepspeed_test_model"
